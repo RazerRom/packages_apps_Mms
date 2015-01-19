cc3c5330e64b58445054f97753c167111adc7611
kaiyiz <kaiyiz@codeaurora.org>
2015-01-19 15:11:50 +0800

    Mms: Fix message notification display unclear issue.
    The text setting of sender in message notification make
    it display unclear on lock screen.
    
    Remove the style used for setting font color.
    
    CRs-Fixed: 782076
    
    Change-Id: I3b34a2d7d176f866b38f4cd14687d03462e65b95

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -726,9 +726,6 @@ public class MessagingNotification {
     // Return a formatted string with all the sender names separated by commas.
     private static CharSequence formatSenders(Context context,
             ArrayList<NotificationInfo> senders) {
-        final TextAppearanceSpan notificationSenderSpan = new TextAppearanceSpan(
-                context, R.style.NotificationPrimaryText);
-
         String separator = context.getString(R.string.enumeration_comma);   // ", "
         SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
         int len = senders.size();
@@ -738,8 +735,7 @@ public class MessagingNotification {
             }
             spannableStringBuilder.append(senders.get(i).mSender.getName());
         }
-        spannableStringBuilder.setSpan(notificationSenderSpan, 0,
-                spannableStringBuilder.length(), 0);
+
         return spannableStringBuilder;
     }
 
