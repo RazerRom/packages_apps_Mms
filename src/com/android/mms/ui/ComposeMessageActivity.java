cbc0b01577606e6dea8f095614402dbb8a37d878
kaiyiz <kaiyiz@codeaurora.org>
2015-02-10 12:23:23 +0800

    Mms: Fix auto exit Mms editor interface when adding picture
    Occur "NullPointerException" when adding over limit picture.
    
    Don't get current slide show size if no exist slideshow.
    
    Change-Id: I633ede225ad30fc88314180766e5865efc7cca83
    CRs-Fixed: 792991

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -3968,8 +3968,9 @@ public class ComposeMessageActivity extends Activity
                 log("resize image " + uri);
             }
             MessageUtils.resizeImageAsync(ComposeMessageActivity.this,
-                    uri, mWorkingMessage.getSlideshow().getCurrentMessageSize(),
-                    mAttachmentEditorHandler, mResizeImageCallback, append);
+                    uri, mWorkingMessage.hasSlideshow() ? mWorkingMessage.getSlideshow()
+                            .getCurrentMessageSize() : 0, mAttachmentEditorHandler,
+                    mResizeImageCallback, append);
             return;
         }
 
