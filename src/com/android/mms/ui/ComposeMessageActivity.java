6a947dd99e445b1a594d6f6eff4c88a5d38cddfd
Danesh M <daneshm90@gmail.com>
2015-07-31 13:10:14 -0700

    Revert "Drafts: Prevent duplicate drafts from being added"
    This reverts commit e63abd6cc1dc254c6410d102a0b382915e1e9310.
    
    Change-Id: Iead0f4b98884da8bd741ffe94e007bc89a8c421c

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -3688,7 +3688,6 @@ public class ComposeMessageActivity extends Activity
                 break;
 
             case REQUEST_CODE_ADD_RECIPIENTS:
-                mShouldLoadDraft = true;
                 mAddNumbersTask = new AddNumbersTask();
                 mAddNumbersTask.execute(
                         data.getStringArrayListExtra(SelectRecipientsList.EXTRA_RECIPIENTS));
@@ -4598,11 +4597,10 @@ public class ComposeMessageActivity extends Activity
      * Load the draft
      *
      * If mWorkingMessage has content in memory that's worth saving, return false.
-     * If mShouldLoadDraft is true, force the previous draft to load no matter what.
      * Otherwise, call the async operation to load draft and return true.
      */
     private boolean loadDraft() {
-        if (mWorkingMessage.isWorthSaving() && !mShouldLoadDraft) {
+        if (mWorkingMessage.isWorthSaving()) {
             Log.w(TAG, "CMA.loadDraft: called with non-empty working message, bail");
             return false;
         }
